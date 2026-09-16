6daabed01ee7955b7f4ed1ab4ab052d49b734487
nattanon12 <junizone@hotmail.com>
2026-09-16 14:46:51 +0700

    Create Exercise4_nattanon_p.py

---
+++ Assignment/Exercise4_nattanon_p.py
@@ -0,0 +1,10 @@
+FA = 3.2     #วิชา Foundation English
+GB = 4        #General Business
+ICS = 3.8     #Introduction to Computer SYSTEM
+CP = 3.5       #Computer Programming
+print(" -- My Score --")
+print("วิชา", "Grade")
+print("Foundation English ได้:", FA)
+print("General Business ได้:", GB)
+print("Introduction to Computer SYSTEM ได้: ", ICS)
+print("Computer Programming ได้:", CP)
