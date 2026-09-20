5987f881102a0f9c8309341d9ef13da64191b828
nattanon12 <junizone@hotmail.com>
2026-09-20 15:41:39 +0700

    Add Exercise5_1_nattanon_p.py

---
+++ Assignment/Exercise5_1_nattanon_p.py
@@ -0,0 +1,13 @@
+#Exercise5_1
+a = 4
+b = 5.5
+c = 6
+d = 7.5
+plus = a + b
+minas = c-b
+times = a*d
+divided = d/a
+print("แสดงผลลัพธ์บวก",plus)
+print("แสดงผลลัพธ์ลบ",minas)
+print("แสดงผลลัพธ์คูณ",times)
+print("แสดงผลลัพธ์หาร",divided)
